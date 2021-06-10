416249ae6f42b4658d89c977320760563b22b16f
Joseph Wiemann <joe.wiemann@gmail.com>
2021-06-09 21:18:47 -0500

    Add project files.

---
+++ Rover2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace RoverApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rover rover = new Rover();
+            int[] inputValues;
+            int width, height;
+       
+            Console.WriteLine("Enter Size Of Field ('5 5'):");
+          
+            string input = Console.ReadLine();
+            if (TryParseIntegerList(input, out inputValues))
+            {
+                if (inputValues.Length == 2)
+                {
+                    width = inputValues[0];
+                    height = inputValues[1];
+                }
+                Console.WriteLine("Enter Position and Direction Rover Is Facing: ('1 2 N'):");
+                input = Console.ReadLine();
+                if(TryParsePositionAndHeading(input, rover))
+                {
+                    Console.WriteLine("Enter Direction and movements for the Rover: ('MMRMMRMRRM");
+                    input = Console.ReadLine();
+                    char command = default;
+                    int i;
+                    for (i = 0; i < input.Length; i++)
+                    {
+                        command = input[i];
+                        if (!Rover.PerformCommand(command, rover))
+                            Console.WriteLine("Command Failed:{0}, {1}, {2}, {3}", command, rover.x, rover.y, rover.heading);
+                    }
+
+                    Console.WriteLine("{1} {2} {3}", command, rover.x, rover.y, rover.heading);
+                }
+            }
+        
+         }
+
+        public static bool TryParseIntegerList(string input, out int[] inputValues)
+        {
+            inputValues = default;
+            string[] splits = input.Split(" ");
+            int[] result = new int[splits.Length];
+            for (int i = 0; i < splits.Length; i++)
+            {
+                if (!int.TryParse(splits[i].Trim(), out result[i]))
+                {
+                    return false;
+                }
+            }
+            inputValues = result;
+            return true;
+        }
+        public static bool TryParsePositionAndHeading(string input, Rover rover)
+        {
+            string[] splits = input.Split(" ");
+
+            if (splits.Length == 3)
+            {
+                int x, y;
+                char heading;
+
+                if (!int.TryParse(splits[0].Trim(), out x))
+                    return false;
+                
+                if (!int.TryParse(splits[1].Trim(), out y))
+                    return false;
+                
+                if (!char.TryParse(splits[2].Trim(), out heading))
+                    return false;
+
+                rover.x = x;
+                rover.heading = heading;
+                rover.y = y;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Rover2/rover.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Rover
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public char heading { get; set; }
+    public Rover()
+	{
+	}
+
+
+    public static bool PerformCommand(char command, Rover rover)
+    {
+        switch (command)
+        {
+            case 'L':
+                if (rover.heading == 'N')
+                    rover.heading = 'W';
+                else if (rover.heading == 'W')
+                    rover.heading = 'S';
+                else if (rover.heading == 'S')
+                    rover.heading = 'E';
+                else if (rover.heading == 'E')
+                    rover.heading = 'N';
+                break;
+            case 'R':
+                if (rover.heading == 'N')
+                    rover.heading = 'E';
+                else if (rover.heading == 'E')
+                    rover.heading = 'S';
+                else if (rover.heading == 'S')
+                    rover.heading = 'W';
+                else if (rover.heading == 'W')
+                    rover.heading = 'N';
+                break;
+            case 'M':
+                if (rover.heading == 'N')
+                    rover.y += 1;
+                if (rover.heading == 'S')
+                    rover.y -= 1;
+                if (rover.heading == 'E')
+                    rover.x += 1;
+                if (rover.heading == 'W')
+                    rover.x -= 1;
+                break;
+
+            default:
+                return false;
+        }
+        return true;
+    }
+}
